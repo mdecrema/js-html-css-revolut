9b0271468d2bb3d24cb1cfd9ead0d6921f7d6685
marco <marcodecrema@libero.it>
2020-09-03 17:30:12 +0200

    mouse over e mouse out

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var menuAzienda = $(".azienda");
-var aziendaHidden = $(".hidden-menu");
+var aziendaHidden = $(".hidden-menu.a");
+var menuTariffazione = $(".tariffazione");
+var tariffazioneHidden = $(".hidden-menu.t");
 
 menuAzienda.mouseover(function() {
   aziendaHidden.addClass("active");
@@ -7,4 +9,12 @@ menuAzienda.mouseover(function() {
 
 menuAzienda.mouseout(function() {
   aziendaHidden.removeClass("active");
+});
+
+menuTariffazione.mouseover(function() {
+  tariffazioneHidden.addClass("active");
+});
+
+menuTariffazione.mouseout(function() {
+  tariffazioneHidden.removeClass("active");
 });
